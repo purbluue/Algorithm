d6672beaa1b4fe59e2b6ae658444580a679f966b
purbluue <143508656+purbluue@users.noreply.github.com>
2024-05-16 20:57:37 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 220 ms, Memory: 17780 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
+		
 		int n = sc.nextInt();
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
+		String s = "";
+		
+		for(int i = 0; i<n; i++) {
+			s += "*";
+			System.out.println(s);
 		}
+
 	}
+
 }
