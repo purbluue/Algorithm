c4cebc12f545176030b8bccb93902a09beeb23de
purbluue <143508656+purbluue@users.noreply.github.com>
2024-05-28 15:10:56 +0900

    [Bronze V] Title: 개수 세기, Time: 148 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -1,25 +1,43 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int count = 0;
-        int N = sc.nextInt();
-        int[] arr = new int[N];				 
-
-        for(int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		StringTokenizer st;
+		
+		//입력
+		int n = Integer.parseInt(br.readLine());
+		
+		int count = 0;
+		int[] arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < n; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        int b = sc.nextInt();
 
-        for(int j = 0; j < arr.length; j++){ 
-            if(b == arr[j]){
+        int b = Integer.parseInt(br.readLine());
+
+        for(int j = 0; j < arr.length; j++){
+            if(b == arr[j]) {
                 count++;
             }
         }
-        System.out.println(count);
-    }
+        bw.write(count + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+
+
+	}
+
 }
